db55029a80b3c044e979ad38ba864933fa88ec71
КленинГолышев <KLENIN2000IGOR@GMAIL.COM>
2024-06-25 00:09:03 +0300

    master

---
+++ LabWorkFunctions/Functions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabWorkFunctions
+{
+    public static class Functions
+    {
+    
+        public static int GetFactorial(int x)
+        {
+            if (x== 0) return 1;
+            if (x<0) return 0;
+            else 
+                return x * GetFactorial(x);
+            throw new NotImplementedException();
+        }
+
+        public static double GetPower(double x, int n)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LabWorkFunctionsTests/FunctionsTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LabWorkFunctions.Tests
+{
+    [TestClass()]
+    public class FunctionsTests
+    {
+        [TestMethod()]
+        [DataTestMethod()]
+        public void FactorialTest_CorrectValues()
+        {
+            int x = 5;
+            int expected = 120;
+
+            int actual = Functions.GetFactorial(x);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void FactorialTest_IncorrectValues()
+        {
+            int x = 0;
+            int expected = -1;
+
+            int actual = Functions.GetFactorial(x);
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod()]
+        [DataTestMethod()]
+        [DataRow(1.5, 2, 2.25)]
+        [DataRow(2, -4, 0.063)]
+        public void PowerTest_PositivePower(double x, int n, double expected)
+        {
+            Assert.AreEqual(expected, Functions.GetPower(x, n), 0.001);
+        }
+
+        [TestMethod()]
+        [DataTestMethod()]
+        public void PowerTest_NegativePower(double x, int n, double expected)
+        {
+            Assert.AreEqual(expected, Functions.GetPower(x, n), 0.001);
+        }
+    }
+}
